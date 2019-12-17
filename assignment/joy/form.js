5fafc29a50551536f56fd6a024bccead2162d6d3
4MJ <54801055+4MJ@users.noreply.github.com>
2019-12-17 11:37:42 +0100

    lesson 44 assignment

---
+++ assignment/joy/form.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('fieldset').append(`
+        <div>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" value="" aria-describedby="name-format" required>
+        </di>
+    `)
+})
