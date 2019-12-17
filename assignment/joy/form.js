cf96699862cdc3ca6d6c6a993c372d4d3f57c91f
4MJ <54801055+4MJ@users.noreply.github.com>
2019-12-17 11:42:27 +0100

    Add files via upload

--- assignment/joy/form.js
+++ assignment/joy/form.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
         <div>
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" value="" aria-describedby="name-format" required>
-        </di>
+        <span id="name-format" class="help">Format </span>
+        </div>
+
     `)
 })
